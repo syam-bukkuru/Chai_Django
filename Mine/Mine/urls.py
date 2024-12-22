2a05d0bfd71274ccd12c85595c2921e444ca54a0
syam-bukkuru <syambukkuru@gmail.com>
2024-12-22 16:12:28 +0530

    Played while integrating Tailwind, hee..

--- Mine/Mine/urls.py
+++ Mine/Mine/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('chai/', include('chai.urls')),
+    
+    path("__reload__", include("django_browser_reload.urls")),
 ]
